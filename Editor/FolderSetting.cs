6e7ce3d5b7ab73b9046008b21e17f716ef4f6b1f
wtl <wtlllll@zju.edu.cn>
2025-12-17 09:46:06 +0800

    [fix] 修复importer显示问题

--- Editor/FolderSetting.cs
+++ Editor/FolderSetting.cs
@@ -15,7 +15,8 @@ namespace AssetFlow
         [SerializeField, Tooltip("是否作用于子文件夹")] public bool includeSubfolders = false;
         public List<string> ExcludeAssetPaths = new();
 
-        [InlineEditor, TabGroup("Importer")] public AssetImporter Importer = new();
+        [InlineEditor(InlineEditorModes.GUIAndHeader), TabGroup("Importer")]
+        public AssetImporter Importer = new();
 
         [TabGroup("Validator")] public List<AssetValidatorBase> Validators = new();
     }
